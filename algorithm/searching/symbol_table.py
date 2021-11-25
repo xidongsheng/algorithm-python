8832929d3a21e3ffe984db747ece30b31aa75c72
Dongsheng Xi <dxi@Tiger.local>
2021-11-25 23:03:25 +0800

    symbol table

--- algorithm/searching/symbol_table.py
+++ algorithm/searching/symbol_table.py
@@ -10,16 +10,55 @@ class SymbolTable(metaclass=abc.ABCMeta):
     def get(self, k):
         pass
 
+    @abc.abstractclassmethod
+    def keys(self):
+        pass
+
+
+class Node(object):
+    def __init__(self, key='', value='', next=None):
+        self.key = key
+        self.value = value
+        self.next = next
+
 
 class SequentialSearchST(SymbolTable):
+    """sequential implement symbol table"""
+
+    def __init__(self):
+        self.first = Node()
 
     def put(self, k, v):
-        print(f'put {k} as {v}')
+        n = self.first
+        while n is not None:
+            if n.key == k:
+                n.value = v
+                return
+            n = n.next
+        
+        self.first = Node(k, v, self.first)
 
     def get(self, k):
-        print(f'get {k}')
+        n = self.first
+        while n is not None:
+            if n.key == k:
+                return n.value
+            n = n.next
+
+        return None
+
+    def keys(self):
+        key_list = []
+        n = self.first
+        while n is not None:
+            key_list.append(n.key)
+            n = n.next
+
+        return key_list
 
 
 st = SequentialSearchST()
-st.put('aa', 'b')
-st.get('a')
+st.put('aa', 'a')
+st.put('bb', 'b')
+print(st.get('aa'))
+print(st.keys())
